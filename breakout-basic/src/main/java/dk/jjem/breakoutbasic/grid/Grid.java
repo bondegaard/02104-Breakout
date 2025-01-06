432a44325d6e9fbfa344e9ceb81f7273333eb0de
skytiger1 <girafbold12@gmail.com>
2025-01-06 16:29:20 +0100

    ser bedre ud og collision hit ting

--- breakout-basic/src/main/java/dk/jjem/breakoutbasic/grid/Grid.java
+++ breakout-basic/src/main/java/dk/jjem/breakoutbasic/grid/Grid.java
@@ -9,38 +9,53 @@ public class Grid {
 
     private Rectangle[][] grid;
 
+    private int offset = 3;
+
     public Grid (PlayScene playScene, int n, int m) {
         grid = new Rectangle[n][m];
 
         // Long side of the rectangle's length, based on window size
-        double lSize = (WindowUtils.getWindowWidth() / m);
+        double lSize = (WindowUtils.getWindowWidth() * (97.5/100.0) / m);
 
         // Short side of the rectangle's length, based on window size
-        double sSize = ((WindowUtils.getWindowHeight() / 20.0) / n);
+        double sSize = ((WindowUtils.getWindowHeight() / 12.5) / n);
 
         // Starting positions, where blank space is calculated
-        double posXStart = WindowUtils.getWindowWidth() * (1/50);
+        double posXStart = WindowUtils.getWindowWidth() * (1.0/100.0);
         double posYStart = posXStart;
 
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < m; col++) {
-
+                // Calculating positions on screen
                 double posX = posXStart + col * lSize; // Horizontal position for each rectangle
                 double posY = posYStart + row * sSize; // Vertical position for each rectangle
 
-                int tempOffset = 3;
-                Rectangle rectangle = new Rectangle(posX, posY, lSize - tempOffset, sSize - tempOffset);
-                rectangle.setX(posX);
-                rectangle.setY(posY);
-
+                // Defining the rectangle
+                Rectangle rectangle = new Rectangle(posX, posY, lSize - offset, sSize - offset);
+                rectangle.setFill(Color.BLUE);
+                
+                // Adding it to grid
                 grid[row][col] = rectangle;
 
-                rectangle.setFill(Color.BLUE);
-
+                // Adding it to the scene
                 playScene.getPane().getChildren().add(rectangle);
+            }
+        }
+    }
 
-                //playScene.getPane().getChildren().remove(rectangle);
-                // Fjern fra grid[][]
+    // If the rectangle got hit, remove it from the scene, and the grid
+    public void rectangleHit(PlayScene playScene, Rectangle r) {
+        if (r == null) return;
+
+        playScene.getPane().getChildren().remove(r);
+       
+        // Removing the rectangle that got hit from the grid
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == r) {
+                    grid[i][j] = null; 
+                    return;
+                }
             }
         }
     }
