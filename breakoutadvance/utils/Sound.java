3c0a8300aa7577911056c568f64efeeda4c593f7
Elias <102224705+bondegaard@users.noreply.github.com>
2025-01-08 09:52:10 +0100

    Update Sound.java

--- breakoutadvance/utils/Sound.java
+++ breakoutadvance/utils/Sound.java
@@ -8,8 +8,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Enum to store all the sounds used in the game
+ */
 public enum Sound {
 
+    // Sound files
     HIT_1("assets/sound/hit1.wav"),
     HIT_2("assets/sound/hit2.wav"),
     HIT_3("assets/sound/hit3.wav"),
@@ -17,10 +21,19 @@ public enum Sound {
     WON("assets/sound/won.wav"),
     LOSE("assets/sound/lose.wav");
 
+    // File name of the sound
     private final String fileName;
+
+    // Map to store the media objects
     private static final Map<Sound, Media> mediaMap = new HashMap<>();
+
+    // MediaPlayer object to play the sound
     private static MediaPlayer mediaPlayer;
 
+    /**
+     * Constructor for the Sound enum
+     * @param fileName The file name of the sound
+     */
     Sound(String fileName) {
         this.fileName = fileName;
     }
@@ -29,10 +42,18 @@ public enum Sound {
         return fileName;
     }
 
+    /**
+     * Get a random hit sound
+     * @return A random hit sound
+     */
     public static Sound getRandomHitSound() {
         return Sound.values()[(int) (Math.random() * 3)];
     }
 
+    /**
+     * Play a sound
+     * @param sound The sound to play
+     */
     public static void playSound(Sound sound) {
         Media media = mediaMap.get(sound);
         if (media != null) {
@@ -44,8 +65,13 @@ public enum Sound {
         }
     }
 
+    /**
+     * PreLoad all the sound files asynchronously
+     */
     public static void load() {
         for (Sound sound : Sound.values()) {
+
+            // Run async so it doesn't block the main thread
             CompletableFuture.runAsync(() -> {
                 try {
                     Media media = new Media(new File(sound.getFileName()).toURI().toString());
