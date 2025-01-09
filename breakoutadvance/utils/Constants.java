e0c10c8ee393ada320d39a1ab3b6bdf64cda0809
Elias <102224705+bondegaard@users.noreply.github.com>
2025-01-09 11:58:06 +0100

    Update Constants.java

--- breakoutadvance/utils/Constants.java
+++ breakoutadvance/utils/Constants.java
@@ -2,7 +2,7 @@ package breakoutadvance.utils;
 
 public class Constants {
 
-    public static final int PADDLE_WIDTH = 100;
+    public static final int PADDLE_WIDTH = 200;
     public static final int PADDLE_HEIGHT = 16;
 
     public static final int BALL_SIZE = 16;
