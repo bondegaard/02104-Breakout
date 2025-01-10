f35a23dc93a10f94bc599f8324a6d88e2d19e0fa
FrostBiil <83719462+FrostBiil@users.noreply.github.com>
2025-01-10 12:33:02 +0100

    Update FontUtil.java

--- breakoutadvance/utils/FontUtil.java
+++ breakoutadvance/utils/FontUtil.java
@@ -8,7 +8,7 @@ public class FontUtil {
     /**
      * Loads the font, if any errors it falls back to a default font Arial.
      */
-    public Font getFont() {
+    public static Font getFont() {
         try (FileInputStream fontStream = new FileInputStream("./assets/fonts/BLACEB__.TTF")) {
             return Font.loadFont(fontStream, 64);
         } catch (Exception e) {
