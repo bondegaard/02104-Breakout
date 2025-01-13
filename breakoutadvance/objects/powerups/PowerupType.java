e97b701ca348b1ae481764845f736427a737582d
Elias <102224705+bondegaard@users.noreply.github.com>
2025-01-13 13:35:58 +0100

    Update PowerupType.java

--- breakoutadvance/objects/powerups/PowerupType.java
+++ breakoutadvance/objects/powerups/PowerupType.java
@@ -11,7 +11,7 @@ import javafx.scene.image.Image;
 public enum PowerupType {
     PLUS_ONE(Images.getImage(Constants.BALL_FILEPATH + Breakout.getInstance().getDataManager().getData().getBallColor() + ".png"), 200), // 1 is equal to 0.1%
     PLUS_WIDTH(Images.getImage(Constants.IMAGE_PATH + "paddleWidthPowerUp.png"), 200),
-    BOMB(Images.getImage( Constants.IMAGE_PATH + "bomb.png"), 1000),
+    BOMB(Images.getImage( Constants.IMAGE_PATH + "bomb.png"), 100),
     HEART(Images.getImage( Constants.IMAGE_PATH + "heart_full.png"), 25);
 
 
